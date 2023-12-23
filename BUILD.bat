86764969a4a07b6e9a2ad230db169c22c7e8c87d
Nico Posada <posadanico@outlook.com>
2023-12-22 19:36:10 -0600

    update batch script to find msbuild path no matter what version of VS2022 you have

--- BUILD.bat
+++ BUILD.bat
@@ -1,9 +1,26 @@
 @echo off
+setlocal enabledelayedexpansion
 
-set MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe"
-set SOLUTION_PATH=GD Hax.sln
-set CONFIGURATION=Release
-set PLATFORM=x86
+set "BasePath=C:\Program Files\Microsoft Visual Studio\2022"
+set "Editions=Community Enterprise Professional"
+set "MSBUILD_PATH="
 
-%MSBUILD_PATH% "%SOLUTION_PATH%" /p:Configuration="%CONFIGURATION%" /p:Platform=%PLATFORM%
+for %%e in (%Editions%) do (
+    set "CheckPath=%BasePath%\%%e"
+    if exist "!CheckPath!\" (
+        set "MSBUILD_PATH=!CheckPath!\Msbuild\Current\Bin\MSBuild.exe"
+        goto :compile
+    )
+)
+
+echo Failed to find Visual Studio 2022 on your system. Make sure to have it installed!
+exit
+
+:compile
+set "SOLUTION_PATH=GD Hax.sln"
+set "CONFIGURATION=Release"
+set "PLATFORM=x86"
+
+"!MSBUILD_PATH!" "!SOLUTION_PATH!" /p:Configuration="!CONFIGURATION!" /p:Platform=!PLATFORM!
 pause
+endlocal
